5e6e79d11658716f3d8b84390915d6796cf2ab70
Manisha Yeramareddy <manisha.yeramareddy@cashcall.com>
2016-09-28 23:01:32 -0700

    frameworks

---
+++ InstaClone/InstaClone-Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  InstaClone-Bridging-Header.h
+//  InstaClone
+//
+//  Created by Manisha Yeramareddy on 9/28/16.
+//  Copyright © 2016 Manisha Yeramareddy. All rights reserved.
+//
+
+#ifndef InstaClone_Bridging_Header_h
+#define InstaClone_Bridging_Header_h
+
+#import <AWSCore/AWSCore.h>
+#import <AWSCognito/AWSCognito.h>
+#import <AWSDynamoDB/AWSDynamoDB.h>
+#import <AWSS3/AWSS3.h>
+
+
+
+
+#endif /* InstaClone_Bridging_Header_h */
